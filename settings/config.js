d30b4930d045396316f17024033f34ade1beebeb
kenshi678 <76278383+kenshi678@users.noreply.github.com>
2021-01-28 15:29:59 -0300

    Update config.js

--- settings/config.js
+++ settings/config.js
@@ -1,6 +1,6 @@
 const config = {
-        botName: 'ToinBOT',
-        ownerName: 'Toin',
-        youtube: 'YOUTUBE_LINK',
+        botName: 'HashiraBOT',
+        ownerName: 'Kenishi comedor de casadas',
+        youtube: 'https://youtube.com/channel/UC9eKenf3rO92IEMsfHk06sQ',
         instagram: 'INSTAGRAM_LINK',
 }
